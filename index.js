a27afa492a3d67f41c953bd0b8ae663f8819771a
Hamza-shah05 <gillanih922@gmail.com>
2024-05-01 17:05:08 +0500

    Adding a Word Counter project

---
+++ index.js
@@ -0,0 +1,23 @@
+#! /usr/bin/env node 
+// Importing inquirer and chalk packages
+import inquirer from "inquirer";
+import chalk from "chalk";
+// Display a colourfull welcome message
+console.log(chalk.white.bold.bgBlue("\n \t Welcome to codiiingwithhamza - word counter\n"));
+console.log("=".repeat(60));
+// prompt the user to enter a sentence
+let answers = await inquirer.prompt([
+    {
+        name: "sentence",
+        type: "Input",
+        message: "Enter a sentence",
+    }
+]);
+// Trimming the  sentence and splitting it into words based on spaces
+let words = answers.sentence.trim().split(" ");
+// Analysis of user Input sentence
+console.log("=".repeat(60));
+console.log(chalk.bold("- Sentence words:"));
+console.log(words);
+console.log(chalk.bold(`\n - word count: ${chalk.bold.redBright(words.length)}`));
+console.log("=".repeat(60));
